4031f3a13ae9220fd24abfcfe17fe03a6bcabe72
ajit <ajit@gmail.com>
2022-02-16 08:08:58 +0530

    new

---
+++ commans.sh
@@ -0,0 +1,18 @@
+[Unit]
+Description=gunicorn daemon
+Requires=gunicorn.socket
+After=network.target
+
+[Service]
+User=ubuntu
+Group=www-data
+WorkingDirectory=/home/ubuntu/project/django_new
+ExecStart=/home/ubuntu/env/bin/gunicorn \
+          --access-logfile - \
+          --workers 3 \
+          --bind unix:/run/gunicorn.sock \
+          educations.wsgi:application
+          
+
+[Install]
+WantedBy=multi-user.target
